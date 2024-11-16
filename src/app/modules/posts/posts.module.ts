555322c334c59b87fd10ba5c6d3b3c0d4efb2f71
salah eddine dik <zettodik@gmail.com>
2024-11-16 01:33:06 +0100

    change deprecated version

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './container/posts.component';
 import { postsRoutingModule } from './posts.routing';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { ServiceApiService } from '../../shared/service/service-api.service'; // Import the service
 
 @NgModule({
   declarations: [PostsComponent],
-  imports: [CommonModule, postsRoutingModule, HttpClientModule], // Import CommonModule for common Angular directives
+  imports: [CommonModule, postsRoutingModule], // Import CommonModule for common Angular directives
   exports: [PostsComponent], // Export PostsComponent so it can be used in other modules
   providers: [
-    ServiceApiService  // Providing the service at the module level
-  ]
+    ServiceApiService,
+    provideHttpClient(), // Providing the HttpClient using hadi badaltha dir deprecated()
+  ],
 })
 export class postsModule {}
