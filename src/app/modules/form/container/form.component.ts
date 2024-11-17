fb963c303047d6288c60fbd2ae9155a8a1855530
salah eddine dik <zettodik@gmail.com>
2024-11-17 17:06:03 +0100

    siba

--- src/app/modules/form/container/form.component.ts
+++ src/app/modules/form/container/form.component.ts
@@ -1,24 +1,35 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';  // Import HttpClient
+import { Observable } from 'rxjs';  // Import Observable
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss',
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.myForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
+  // Submit form data to the JSON server
   onSubmit() {
     if (this.myForm.valid) {
-      console.log('Form submitted:', this.myForm.value);
+      const formData = this.myForm.value;
+      this.saveUserData(formData).subscribe(response => {
+        console.log('Form data saved:', response);
+      });
     }
+  }
+
+  // Method to send form data to the JSON server
+  saveUserData(data: any): Observable<any> {
+    return this.http.post('http://localhost:3000/users', data);
   }
 }
--- src/app/modules/form/form.module.ts
+++ src/app/modules/form/form.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormRoutingModule } from './from.routing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from './container/form.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [FormComponent],
   imports: [CommonModule, FormRoutingModule, ReactiveFormsModule],
+  providers: [
+    provideHttpClient(), // Providing the HttpClient using hadi badaltha dir deprecated()
+  ]
 })
 export class FormModule {}
