e6e57ddae61b97bfea980d32e3e775c03d2f1b32
salah eddine dik <zettodik@gmail.com>
2024-11-21 19:09:26 +0100

    make new add tow databydin

--- src/app/modules/home/components/resultform/resultform.component.ts
+++ src/app/modules/home/components/resultform/resultform.component.ts
@@ -6,6 +6,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./resultform.component.scss'],
 })
 export class ResultFormComponent {
+  elementsh2 = 'titite get text from input';
+
+  buttonfunction(newInput: string) {
+    this.elementsh2 = newInput; // Access the value of the input element
+  }
+
+  elementshtow = 'titite with tow yay data binding';
+  buttonfunctiontow(newInput: string) {
+    this.elementsh2 = newInput; // Access the value of the input element
+  }
   @Input() resultvariable: { input1: string; input2: string } | null = null;
   amount = 123.45;
   company = 'acme corporation';
